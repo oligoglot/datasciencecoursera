d6f3bb1baa3ba9cab3526a3eb0ec7d424c537a0e
Sundar <sundar@zettata.com>
2014-09-21 09:07:15 +0530

    added comments to data preparation script

--- datacleaning/3/prepare.sh
+++ datacleaning/3/prepare.sh
@@ -1,4 +1,6 @@
+# download data
 wget https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 unzip getdata_projectfiles_UCI\ HAR\ Dataset.zip
 mv UCI\ HAR\ Dataset/ Dataset
+# remove leading and trailing whitespaces and replace a string of spaces with one
 perl -i -ne 's/^\s*//; s/\s+/ /g; s/\s$//; print "$_\n";' Dataset/t*/X_*.txt
